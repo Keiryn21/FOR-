bcdc37a00f0a1c2c38c2fc8e012b78738e6c09e8
Keiryn21 <00061225@uca.edu.sv>
2025-06-06 14:29:25 -0600

    10

---
+++ exercise1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+    //contador del 1 al 10//
+
+    for (int i = 0; i <=10; i++)
+    {
+        cout << i << endl;
+    }
+    cout<<"numeros del 1 al 10"<<endl;
+    
+    return 0;
+}
