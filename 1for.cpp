2d331bebca5baff82b2f3e01130c6f2677735f8a
Keiryn21 <00061225@uca.edu.sv>
2025-06-06 14:04:20 -0600

    practicas

---
+++ 1for.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    for (int i = 0; i <=10; i++)
+    {
+        cout<<"practica de for"<<endl;
+    }
+
+
+    return 0;
+}
---
+++ ejercicio2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    for (int i = 0; i <=10; i++)
+    {
+        cout<< i << endl;
+        cout<<"practica 2"<<endl;
+    }
+    
+
+cout<<"fin del programa"<<endl;
+    return 0;
+}
