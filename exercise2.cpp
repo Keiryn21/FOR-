9b4a18227e7d2d05fbc632d9731907884c8d9547
Keiryn21 <00061225@uca.edu.sv>
2025-06-06 14:35:35 -0600

    50

---
+++ exercise2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    //ejercicio 2,numeros pares del 1 al 50//
+
+    for (int i = 0; i <=50; i+=2)
+    {
+        cout<< i <<endl;
+    }
+    
+    cout<<"numeros pares";
+
+    return 0;
+}
