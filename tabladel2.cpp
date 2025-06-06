c2187af8c8b78b76cc5acf242ffc176ae1a39594
Keiryn21 <00061225@uca.edu.sv>
2025-06-06 17:02:34 -0600

    tabla del 2

---
+++ tabladel2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+    //tabla del 2//
+    int numero=2;
+
+    for (int i = 1; i <=10; i++)
+    {
+        cout<<"tabla del 2"<<endl;
+        cout<<numero<< "x"<< i << "="<< (numero*i)<<endl;
+    }
+    
+    return 0;
+}
